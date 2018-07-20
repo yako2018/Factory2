2bee98557359f576acafc9ff3d2bf37b31b6b596
Yako.ko <yako.ko@cloudnifier.com>
2018-07-20 15:10:06 +0800

    ADD PHONE

---
+++ BuildingBlocks/BuildingBlocks/Models/Phone.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildingBlocks.Service.Crm
+{
+    public class Phone : ICrmPhone
+    {
+        public string clf_contact { set; get; }
+        public string name { set; get; }
+        public string number { set; get; }
+        public string phoneid { set; get; }
+    }
+}
