c61b2f18285d87e931151f90d75314d9a1cfc667
Yako.ko <yako.ko@cloudnifier.com>
2018-07-21 10:08:38 +0800

    api

---
+++ BuildingBlocks/BuildingBlocks/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using BuildingBlocks.Models;
+using BuildingBlocks.Service.Crm;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BuildingBlocks.Controllers
+{
+    public class PersonController : ApiController
+    {
+        //http://localhost:2568/Person/Detail
+        [Route("Person/Detail")]
+        [HttpGet]
+        public string personDetail()
+        {
+            //string jsonString = @"{'status':500}";
+            try
+            {
+                PhoneHandler phoneHandler = new PhoneHandler();
+                Phone phone = phoneHandler.TestPhone();
+                ResultModel resultModel = new ResultModel();
+                resultModel.Message = phone;
+                var s = JsonConvert.SerializeObject(resultModel);
+                return s;
+            }
+            catch (Exception ex)
+            {
+                ResultModel resultModel = new ResultModel();
+                resultModel.IsSucess = false;
+                resultModel.Message = ex;
+                var s = JsonConvert.SerializeObject(resultModel);
+                return s;
+            }
+            
+
+            //return jsonString;
+        }
+    }
+}
+
---
+++ BuildingBlocks/BuildingBlocks/Models/ResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildingBlocks.Models
+{
+    public class ResultModel
+    {
+        public bool IsSucess { get; set; }
+        public object Message { get; set; }
+    }
+}
